2803e3154f5ae705cb9b2ba8ffedbd0e7b4f2415
Boyd Rehorst <boyd.rehorst@ziggo.nl>
2020-09-16 19:05:30 +0200

    fix(apollo): use latest apollo client version

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,40 +1,9 @@
-import { ApolloClient } from "apollo-client";
-import {
-  InMemoryCache,
-  IntrospectionFragmentMatcher,
-} from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client/core";
 import fetch from "node-fetch";
 import { WiremockStubGeneratorConfig } from "./config";
 
-const introspectionQuery = async (config: WiremockStubGeneratorConfig) => {
-  const request = fetch(config.proxy.schema, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      variables: {},
-      query: `
-      {
-        __schema {
-          types {
-            kind
-            name
-            possibleTypes {
-              name
-            }
-          }
-        }
-      }
-    `,
-    }),
-  });
-  const response = await request;
-  const result = await response.json();
-  return result.data;
-}
-
 export const getClient = async (
-  config: WiremockStubGeneratorConfig,  
+  config: WiremockStubGeneratorConfig,
 ) =>
   new ApolloClient({
     link: new HttpLink({
@@ -43,11 +12,5 @@ export const getClient = async (
       fetch,
       headers: config.proxyHeaders,
     }),
-    cache: new InMemoryCache({
-      fragmentMatcher: new IntrospectionFragmentMatcher(        
-        {
-          introspectionQueryResultData: await introspectionQuery(config)
-        }
-      ),
-    }),
+    cache: new InMemoryCache(),
   });
